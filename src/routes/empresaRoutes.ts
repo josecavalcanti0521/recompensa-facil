4967ce3edd77c9901a62bf47351cc253ea147b4c
José Cavalcanti <josemarcelo0521@gmail.com>
2025-07-24 23:32:16 -0300

    refactor: change the way company is updated, add selection of provided id and token id

--- src/routes/empresaRoutes.ts
+++ src/routes/empresaRoutes.ts
@@ -11,11 +11,11 @@ router.post('/login', empresaController.login.bind(empresaController));
 router.post('/register-user', verifyAuth, empresaController.registerUser.bind(empresaController));
 router.post('/register-compra/:empresaId', verifyAuth, empresaController.registerCompra.bind(empresaController));
 router.get('/find-all-compras/:empresaId', verifyAuth, empresaController.findAllCompraById.bind(empresaController))
+router.patch('/:id', verifyAuth, empresaController.update.bind(empresaController));
 router.get('/', empresaController.findAll.bind(empresaController));
 
 router.get('/id/:id', empresaController.findById.bind(empresaController));
 router.get('/cnpj/:cnpj', empresaController.findByCnpj.bind(empresaController));
-router.patch('/:id', empresaController.update.bind(empresaController));
 router.delete('/:id', verifyAuth, empresaController.delete.bind(empresaController));
 
 export default router;
